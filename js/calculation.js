ea2662770ee43454453bfcb67919219fb8854636
Programmerbikash <bikash19081998dey@gmail.com>
2022-08-23 10:19:40 +0600

    calcultaion design complete

---
+++ js/calculation.js
@@ -0,0 +1,31 @@
+function inputValue(value){
+    const inputAmount = document.getElementById(value);
+    const previousInputAmount = inputAmount.value;
+    const inputAmountNext = parseInt(previousInputAmount);
+    return inputAmountNext;
+}
+
+document.getElementById('per-player-field').addEventListener('click', function(){
+    const playerInputAmount = inputValue('player-input');
+    
+    const playerExpensesField = document.getElementById('player-expenses-field');
+    const playerExpenses = playerExpensesField.innerText;
+    playerExpensesField.innerText = playerInputAmount;
+})
+
+document.getElementById('total-calculation').addEventListener('click', function(){
+    const playerInputAmount = inputValue('player-input');
+
+    const managerAmount = inputValue('manager-amount');
+
+    const coachAmount = inputValue('coach-amount');
+
+    const totalAmount = playerInputAmount + managerAmount + coachAmount;
+    console.log(totalAmount)
+
+    const totalAmountButton = document.getElementById('total-amount');
+    const previousTotalAmountButton = totalAmountButton.innerText;
+
+    return totalAmountButton.innerText = totalAmount;
+})
+
